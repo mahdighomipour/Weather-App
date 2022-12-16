c5b2e6a4f9e44dbaac45c3c68ce5cad30cdc9cab
Mahdi Ghomipour <mehdiqomeepour@gmail.com>
2022-12-16 20:46:06 +0330

    add some data and relevant icon from API

--- src/app.js
+++ src/app.js
@@ -5,11 +5,16 @@ function toInsert(showCity) {
 	let wind = document.querySelector("#wind");
 	let desc = document.querySelector("#description");
 	let currentTemp = Math.round(showCity.data.main.temp);
+	let iconSrc = document.querySelector("#icon");
 	insertCity.innerHTML = `${currentTemp}`;
 	hem.innerHTML = `${showCity.data.main.humidity}`;
 	wind.innerHTML = `${showCity.data.wind.speed}`;
 	cityName.innerHTML = `${showCity.data.name}`;
 	desc.innerHTML = `${showCity.data.weather[0].description}`;
+	let iconSource = `http://openweathermap.org/img/wn/${showCity.data.weather[0].icon}@2x.png`;
+	iconSrc.setAttribute("src", iconSource);
+	iconSrc.setAttribute("alt", showCity.data.weather[0].description);
+	console.log(iconSource);
 }
 
 function start(event) {
