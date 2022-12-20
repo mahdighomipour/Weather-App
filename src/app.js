0cdf275e354ccf541026f39c6077f9030f796a04
Mahdi Ghomipour <mehdiqomeepour@gmail.com>
2022-12-20 10:09:06 +0330

    API and other implementation

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
+function toInsertForecast(response) {
+	let days = ["Fri", "Sat", "Sun", "Mon", "Tue", "Wed", "Thu"];
+	let insertToDivForecast = document.querySelector("#forecastSection");
+	let forecastObject = `<div class="row">`;
+	days.forEach(function (day) {
+		forecastObject += `<div class="col-2">
+			${day}
+			<img src="https://ssl.gstatic.com/onebox/weather/64/fog.png" alt="" />
+			<span class="min">min</span>
+			<span class="max">max</span>
+			</div>`;
+	});
+	forecastObject += `</div>`;
+	insertToDivForecast.innerHTML = forecastObject;
+	console.log(response.data.daily);
+}
+
 function toInsert(showCity) {
 	let insertCity = document.querySelector("#cityCurrentTemp");
 	let cityName = document.querySelector("#cityName");
@@ -14,6 +31,11 @@ function toInsert(showCity) {
 	let iconSource = `http://openweathermap.org/img/wn/${showCity.data.weather[0].icon}@2x.png`;
 	iconSrc.setAttribute("src", iconSource);
 	iconSrc.setAttribute("alt", showCity.data.weather[0].description);
+	lat = showCity.data.coord.lat;
+	lon = showCity.data.coord.lon;
+
+	let end2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+	axios.get(end2).then(toInsertForecast);
 }
 
 function start(event) {
@@ -21,7 +43,6 @@ function start(event) {
 	let city3 = document.querySelector("#search-name-form");
 	let cityName = city3.value;
 	let endPiont = "https://api.openweathermap.org/data/2.5/weather?q=";
-	let apiKey = "f09d3949047ab6c9e3bcaf79cf61f619";
 	let units = "metric";
 	axios
 		.get(`${endPiont}${cityName}&appid=${apiKey}&units=${units}`)
@@ -60,3 +81,4 @@ clickFahrenheitLink.addEventListener("click", tofahrenheit);
 
 let activeLinkF = document.querySelector("#fahrenheitLink");
 let activeLinkC = document.querySelector("#celsiusLink");
+let apiKey = "62231151ce343c4d68652e1617efc22f";
